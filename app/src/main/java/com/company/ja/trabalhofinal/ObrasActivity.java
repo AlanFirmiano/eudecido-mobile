71f2c133fdd4badf3a90ba3be4a60eb41c6327b4
AlanFirmiano <alanfirmiano18@gmail.com>
2018-11-11 20:54:14 -0300

    list obras

--- app/src/main/java/com/company/ja/trabalhofinal/ObrasActivity.java
+++ app/src/main/java/com/company/ja/trabalhofinal/ObrasActivity.java
@@ -4,14 +4,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.company.ja.trabalhofinal.model.Obra;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ObrasActivity extends Activity {
-
+    ListView listView;
+    private List<Obra> obras = new ArrayList<>();
+    private ArrayAdapter<Obra> arrayAdapter;
+    int myPosition = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_obras);
+        listView = (ListView) findViewById(R.id.listObras);
+
+        carregar();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                myPosition = position;
+                view.setSelected(true);
+            }
+
+        });
+    }
+
+    public void carregar(){
+        Obra ob = new Obra();
+        ob.setDescricao("OBRA1");
+        obras.add(ob);
+        Obra ob2 = new Obra();
+        ob.setDescricao("OBRA2");
+        obras.add(ob2);
+        listView.setAdapter(arrayAdapter);
+        arrayAdapter = new ArrayAdapter<Obra>(ObrasActivity.this,android.R.layout.simple_list_item_1, obras);
     }
 
 }
