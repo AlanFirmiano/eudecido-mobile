050fcf68b7b6732178259a4db42da12171d6d78c
Joao Farias <jofarias98@gmail.com>
2018-11-29 11:15:58 -0300

    Login Facebook

--- app/src/main/java/com/company/ja/trabalhofinal/LoginActivity.java
+++ app/src/main/java/com/company/ja/trabalhofinal/LoginActivity.java
@@ -1,34 +1,78 @@
 package com.company.ja.trabalhofinal;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+public class LoginActivity extends Activity {
+
+    private FirebaseAuth mAuth;
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_login);
 
-        Intent intent = new Intent();
 
-        Button button = findViewById(R.id.button);
-        Button buttonR = findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                menu();
-            }
-        });
-        buttonR.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                register();
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
+        mCallbackManager = CallbackManager.Factory.create();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        LoginManager.getInstance().registerCallback(mCallbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.d("AQUI", "AQUI");
+                        menu();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.d("AQUI", "AQUI");
+                        menu();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Log.d("AQUI", "AQUI");
+                        menu();
+                    }
+                });
+        LoginManager.getInstance().logOut();
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email"));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void menu(){
